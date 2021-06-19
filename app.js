80e84062b8c8833cef945383e9328fe6e40d9182
Guilherme Sales <guilherme.sales@inf.ufrgs.br>
2021-06-19 12:49:00 -0300

    user connected

--- app.js
+++ app.js
@@ -13,4 +13,11 @@ const server = http.createServer(app);
 
 const io = socketIo(server);
 
+io.on("connection", socket => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("a user disconected");
+  })
+})
+
 server.listen(port, () => console.log(`Listening on port ${port}`));
