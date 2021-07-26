323a593013be2d5e57b095dc59fb9f7056d0388e
Guilherme Sales <guilherme.sales@inf.ufrgs.br>
2021-07-26 18:21:23 -0300

    WIP

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
 require('dotenv').config();
 const express = require("express");
-const http = require("http");
 const socketIo = require("socket.io");
+var bodyParser = require("body-parser");
+var path = require("path")
+var uuid = require('uuid-random');
+const { uniqueNamesGenerator, adjectives, colors, animals, names } = require('unique-names-generator');
 
 const port = process.env.PORT || 4001;
 const index = require('./routes/index');
 
 const app = express();
+
+var server = app.listen(port, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Listening at http://%s:%s', 'localhost:', port);
+});
+
 app.use(index);
+app.use(express.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ['GET', 'POST']
+  }
+});
 
-const io = socketIo(server);
+var chatRoomData = [];
+var connectedClients = {};
 
 io.on("connection", socket => {
-  console.log("a user connected");
+  console.log("a user connected", socket);
+  socket.emit("FromAPI", new Date());
   socket.on("disconnect", () => {
     console.log("a user disconected");
   })
 })
 
-server.listen(port, () => console.log(`Listening on port ${port}`));
